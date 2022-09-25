a09c4dca9e8e53f4353f73fc18dc0f36719622a5
Norbee <n.horox@gmail.com>
2022-09-25 12:38:56 +0200

    Opt in to use react-router 6.4 features

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,57 @@
 import { lazy, Suspense } from "react"
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 
 import { LoadingOverlay } from "@mantine/core"
 
-import { Footer } from "./components"
 import { AuthenticatedRoute } from "./components/AuthenticatedRoute"
 import { AppRoutes } from "./config/app-routes"
 import { AppMantineProvider } from "./context/AppMantineProvider"
 import { AuthProvider } from "./context/AuthProvider"
 import { AvailabilityProvider } from "./context/AvailabilityProvider"
+import { PageLayout } from "./pages/PageLayout"
 
 const StartPage = lazy(() => import("./pages/StartPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage"));
 const AboutPage = lazy(() => import("./pages/AboutPage"));
 const LoginRedirectPage = lazy(() => import("./pages/LoginRedirectPage"));
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<PageLayout />}>
+      <Route
+        index
+        element={
+          <AuthenticatedRoute>
+            <AvailabilityProvider>
+              <StartPage />
+            </AvailabilityProvider>
+          </AuthenticatedRoute>
+        }
+      />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="redirect" element={<LoginRedirectPage />} />
+      <Route path="*" element={<Navigate to={AppRoutes.Start} />} />
+    </Route>
+  )
+);
+
 export const App = () => {
   return (
     <AuthProvider>
       <AppMantineProvider>
-        <BrowserRouter>
-          <Suspense
-            fallback={
-              <LoadingOverlay
-                visible
-                loaderProps={{ size: 50, variant: "bars" }}
-                transitionDuration={500}
-              />
-            }
-          >
-            <Routes>
-              <Route
-                path={AppRoutes.Start}
-                element={
-                  <AuthenticatedRoute>
-                    <AvailabilityProvider>
-                      <StartPage />
-                    </AvailabilityProvider>
-                  </AuthenticatedRoute>
-                }
-              />
-              <Route path={AppRoutes.Login} element={<LoginPage />} />
-              <Route path={AppRoutes.About} element={<AboutPage />} />
-              <Route
-                path={AppRoutes.LoginRedirect}
-                element={<LoginRedirectPage />}
-              />
-              <Route path="*" element={<Navigate to={AppRoutes.Start} />} />
-            </Routes>
-          </Suspense>
-          <Footer />
-        </BrowserRouter>
+        <Suspense
+          fallback={
+            <LoadingOverlay
+              visible
+              loaderProps={{ size: 50, variant: "bars" }}
+              transitionDuration={500}
+            />
+          }
+        >
+          <RouterProvider router={router} />
+        </Suspense>
       </AppMantineProvider>
       <ToastContainer
         position="bottom-center"
